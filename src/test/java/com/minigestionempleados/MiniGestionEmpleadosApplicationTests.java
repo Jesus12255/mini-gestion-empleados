4f054517b8797c0859a98a15f9bef9423d0b78de
admin <jh949742@gmail.com>
2024-01-20 14:51:44 -0500

    creacion del proyecto

---
+++ src/test/java/com/minigestionempleados/MiniGestionEmpleadosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.minigestionempleados;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniGestionEmpleadosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
